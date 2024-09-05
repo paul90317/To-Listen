167c379c3a91a41afad33154e48ef55201617793
Paul Wu <paul2309825@gmail.com>
2024-09-05 17:52:18 +0800

    Implement music playing by web stream

--- YouTube Background/ViewController.swift
+++ YouTube Background/ViewController.swift
@@ -10,15 +10,43 @@ import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var audio_id: UITextField!
-    
+    var player:AVPlayer?
+    var playing:Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
     @IBAction func paly(_ sender: Any) {
-        audio_id.text = "";
+        if let player = player {
+            if playing {
+                player.pause()
+                playing = false
+            } else {
+                player.play()
+                playing = true
+            }
+        } else {
+            guard let text = audio_id.text, let url = URL(string: text) else {
+                print("unvalid URL")
+                return
+            }
+            do {
+                try AVAudioSession.sharedInstance().setMode(.default)
+                try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                player = AVPlayer(url: url)
+                guard let player = player else {
+                    print("can't visit URL")
+                    return
+                }
+                playing = true
+                player.play()
+            }catch{
+                print("can't play the music")
+            }
+        }
     }
+    
     
 }
 
