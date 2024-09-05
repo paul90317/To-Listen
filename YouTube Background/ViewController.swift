0940a8de4d67ce17edf5d7a59774cc3a7ca816eb
Paul Wu <paul2309825@gmail.com>
2024-09-05 23:15:57 +0800

    Implement Picture in Picture

--- YouTube Background/ViewController.swift
+++ YouTube Background/ViewController.swift
@@ -6,47 +6,54 @@
 //
 
 import UIKit
-import AVFoundation
+import AVKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var audio_id: UITextField!
-    var player:AVPlayer?
-    var playing:Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
-    
-    @IBAction func paly(_ sender: Any) {
-        if let player = player {
-            if playing {
-                player.pause()
-                playing = false
-            } else {
-                player.play()
-                playing = true
-            }
-        } else {
-            guard let text = audio_id.text, let url = URL(string: text) else {
-                print("unvalid URL")
-                return
-            }
-            do {
-                try AVAudioSession.sharedInstance().setMode(.default)
-                try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
-                player = AVPlayer(url: url)
-                guard let player = player else {
-                    print("can't visit URL")
-                    return
-                }
-                playing = true
-                player.play()
-            }catch{
-                print("can't play the music")
-            }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+        }catch{
+            print("can't play the music")
         }
     }
     
-    
+    @IBAction func paly(_ sender: Any) {
+        
+        guard let text = audio_id.text, let url = URL(string: text) else {
+            print("unvalid URL")
+            return
+        }
+        
+        let asset = AVAsset(url: url)
+        
+        Task {  
+            do {
+                // 使用新的 async/await 加载属性
+                try await asset.load(.duration)
+                
+                let duration = CMTimeGetSeconds(asset.duration) / 2
+                
+                // 创建播放器
+                let playerItem = AVPlayerItem(asset: asset)
+                playerItem.forwardPlaybackEndTime = CMTime(seconds: duration, preferredTimescale: 600)
+                
+                let player = AVPlayer(playerItem: playerItem)
+                
+                // 创建播放器控制器
+                let playerViewController = AVPlayerViewController()
+                playerViewController.player = player
+                
+                // 显示播放器并开始播放
+                self.present(playerViewController, animated: true) {
+                    player.play()
+                }
+            } catch {
+                print("Failed to load duration: \(error)")
+            }
+        }
+        
+    }
 }
 
