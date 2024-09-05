94a63f2a81b213fe0d718a2574d47d8f4b8e5f6e
Paul Wu <paul2309825@gmail.com>
2024-09-05 17:02:20 +0800

    add TextField and Button

--- YouTube Background/ViewController.swift
+++ YouTube Background/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var audio_id: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func paly(_ sender: Any) {
+        audio_id.text = "";
+    }
+    
 }
 
