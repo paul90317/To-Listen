6286e586d1c5116b002208988bc8d2f5c626650c
Paul Wu <paul2309825@gmail.com>
2024-09-17 16:52:05 +0800

    Implement play control

--- To Listen List/CDPlayerView.swift
+++ To Listen List/CDPlayerView.swift
@@ -14,6 +14,8 @@ struct CDPlayer: View {
     @State private var current_audio_id :Int = 0
     @State private var notificationHandlers: [NSObjectProtocol] = []
     @State private var player = AVPlayer()
+    @State private var isPlaying :Bool = true
+    @State private var KVOs: [NSKeyValueObservation] = []
     private let first_audio_id: Int
     static let resetPlayerEvent = Notification.Name("resetPlayerEvent")
     init (audio_id: Int) {
@@ -22,43 +24,111 @@ struct CDPlayer: View {
     }
     
     var body: some View {
-        VideoPlayer(player: player)
-            .navigationBarBackButtonHidden(false) // 强制显示返回按钮
-            .navigationBarTitleDisplayMode(.automatic)
-            .onDisappear {
-                print("bye")
-                for handler in notificationHandlers {
-                    NotificationCenter.default.removeObserver(handler)
-                }
-                notificationHandlers.removeAll()
-                player.replaceCurrentItem(with: nil)
+        VStack {
+            if let image = UIImage(data: items[current_audio_id].image) {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.horizontal)
+                    .cornerRadius(10)
+                    .padding(.top, 20)
             }
-            .onAppear {
-                print("hi")
-                notificationHandlers.append(NotificationCenter.default.addObserver(
-                    forName: CDPlayer.resetPlayerEvent,
-                    object: nil,
-                    queue: .main
-                ){_ in
-                    print("play")
-                    Task {
-                        let newItem = try await getAVPlayerItem(item: items[current_audio_id])
-                        player.replaceCurrentItem(with: newItem)
+            
+            // Song information
+            VStack(alignment: .leading) {
+                Text(items[current_audio_id].title)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .padding(.top, 10)
+                
+                Text(items[current_audio_id].artist)
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+                    .padding(.top, 2)
+            }
+            .padding(.horizontal)
+            
+
+            // Playback controls
+            HStack(spacing: 40) {
+                Button(action: {
+                    current_audio_id = (current_audio_id - 1  + items.count) % items.count
+                    NotificationCenter.default.post(name: CDPlayer.resetPlayerEvent, object: nil)
+                }) {
+                    Image(systemName: "backward.fill")
+                        .font(.largeTitle)
+                }
+                
+                Button(action: {
+                    if isPlaying {
+                        player.pause()
+                    } else {
                         player.play()
                     }
-                })
-                notificationHandlers.append(NotificationCenter.default.addObserver(
-                    forName: .AVPlayerItemDidPlayToEndTime,
-                    object: nil,
-                    queue: .main
-                ) { _ in
-                    print("next")
+                }) {
+                    Image(systemName: isPlaying ? "pause.fill" : "play.fill")
+                        .font(.largeTitle)
+                }
+                
+                Button(action: {
                     current_audio_id = (current_audio_id + 1) % items.count
                     NotificationCenter.default.post(name: CDPlayer.resetPlayerEvent, object: nil)
-                })
-                
-                current_audio_id = first_audio_id
-                NotificationCenter.default.post(name: CDPlayer.resetPlayerEvent, object: nil)
+                }) {
+                    Image(systemName: "forward.fill")
+                        .font(.largeTitle)
+                }
             }
+            .padding(.top, 30)
+            
+            VideoPlayer(player: player)
+                .frame(height: 0)
+                .onDisappear {
+                    print("bye")
+                    for handler in notificationHandlers {
+                        NotificationCenter.default.removeObserver(handler)
+                    }
+                    notificationHandlers.removeAll()
+                    for handler in KVOs {
+                        handler.invalidate()
+                    }
+                    KVOs.removeAll()
+                    
+                    player.replaceCurrentItem(with: nil)
+                                    }
+                .onAppear {
+                    print("hi")
+                    notificationHandlers.append(NotificationCenter.default.addObserver(
+                        forName: CDPlayer.resetPlayerEvent,
+                        object: nil,
+                        queue: .main
+                    ){_ in
+                        print("play")
+                        Task {
+                            let newItem = try await getAVPlayerItem(item: items[current_audio_id])
+                            player.replaceCurrentItem(with: newItem)
+                            player.play()
+                        }
+                    })
+                    notificationHandlers.append(NotificationCenter.default.addObserver(
+                        forName: .AVPlayerItemDidPlayToEndTime,
+                        object: nil,
+                        queue: .main
+                    ) { _ in
+                        print("next")
+                        current_audio_id = (current_audio_id + 1) % items.count
+                        NotificationCenter.default.post(name: CDPlayer.resetPlayerEvent, object: nil)
+                    })
+                    KVOs.append(player.observe(\.rate, options: [.new]) { _, change in
+                        guard let rate = change.newValue else {
+                            return
+                        }
+                        print("change")
+                        isPlaying = rate > 0
+                    })
+                    
+                    current_audio_id = first_audio_id
+                    NotificationCenter.default.post(name: CDPlayer.resetPlayerEvent, object: nil)
+                }
         }
+    }
 }
